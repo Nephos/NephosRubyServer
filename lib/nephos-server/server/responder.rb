b4ddcc2de8105e287d0c6db84dfec93bc0a73d71
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-08-23 16:56:38 +0200

    improve responder
    - fixes
    - documentation
    - rename some functions

--- lib/nephos-server/server/responder.rb
+++ lib/nephos-server/server/responder.rb
@@ -1,22 +1,30 @@
 module Nephos
   module Responder
 
+    class InvalidContentType < StandardError; end
+
     CT_CHARSET_PREFIX = '; charset='
 
     def self.content_type(kind, type, charset='UTF-8')
       {'Content-type' => "#{kind}/#{type}" + CT_CHARSET_PREFIX + charset}
     end
-
-    def self.plain
+    def self.ct_plain
       content_type(:text, :plain)
     end
-    def self.html
+    def self.ct_html
       content_type(:text, :html)
     end
-    def self.json
+    def self.ct_json
       content_type(:text, :javascript)
     end
+    # @param params [Hash] containing :type => "kind/type", example: "text/html"
+    def self.ct_specific(params)
+      kind, type = params[:type].match(/^(\w+)\/(\w+)$/)[1..2]
+      raise InvalidContentType, "params[:type] must match with \"kind/type\"" if kind.nil? or type.nil?
+      content_type(kind, type)
+    end
 
+    # Fill params with default parameters (status, plain errors)
     def self.set_default_params params
       if (params.keys & [:status]).empty?
         params[:status] ||= 200
@@ -31,12 +39,22 @@ module Nephos
       params
     end
 
+    # @return [Symbol, nil] :plain, :html, :json, or nil
+    # search the content type
     def self.params_content_type params
       (params.keys & [:plain, :html, :json]).first
     end
 
+    # search the content to render from the params,
+    # based on content_type (plain, html, ...).
+    # If not, check for specific type
     def self.params_content_type_value params
-      self.send(params_content_type(params))
+      type = params_content_type(params)
+      if type
+        self.send("ct_#{type}")
+      else
+        self.send("ct_specific", params)
+      end
     end
 
     # @param params [Hash, Symbol]
