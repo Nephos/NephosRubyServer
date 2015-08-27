6d59c17e49b3ea680b55f473e251b6152f3636bf
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-08-27 21:34:26 +0200

    add unit test on params

--- test/params.rb
+++ test/params.rb
@@ -1,2 +1,14 @@
 class TestNephosServerParams < Test::Unit::TestCase
+
+  def test_basic
+    p = Nephos::Params.new
+    assert p.empty?
+    p[:a] = "1"
+    assert_equal p[:a], "1"
+    assert_equal p["a"], "1"
+    p["a"] = "2"
+    assert_equal p[:a], "2"
+    assert_equal p["a"], "2"
+  end
+
 end
