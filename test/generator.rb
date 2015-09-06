b954e5c594c5a644e057407a82c7e66c0de57e3e
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-09-07 00:21:40 +0200

    add new test on route generator

--- test/generator.rb
+++ test/generator.rb
@@ -20,7 +20,8 @@ class TestNephosServerGenerator < Test::Unit::TestCase
     `rm -f app/test.rb`
   end
 
-  def test_generator_route
+  # test simple and rm
+  def test_generator_route1
     s1 = File.read("routes.rb")
     `./bin/nephos-generator --debug r get test ctr#mth`
     s2 = File.read("routes.rb")
@@ -28,6 +29,19 @@ class TestNephosServerGenerator < Test::Unit::TestCase
     s3 = File.read("routes.rb")
     assert_equal s1, s3
     assert_not_equal s1, s2
+  end
+
+  # test if ctr#mth == ctr mth
+  def test_generator_route2
+    s1 = File.read("routes.rb")
+    `./bin/nephos-generator --debug r get test ctr#mth`
+    s2 = File.read("routes.rb")
+    `./bin/nephos-generator --debug r get test ctr mth`
+    s3 = File.read("routes.rb")
+    `./bin/nephos-generator --debug r get test ctr mth --rm`
+    s4 = File.read("routes.rb")
+    assert_equal s2, s3
+    assert_equal s1, s4
   end
 
 end
