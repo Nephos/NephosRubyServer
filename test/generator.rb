e23901f7cb63765f080bb40d0f36186b7757d008
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-09-08 23:49:29 +0200

    fix tests on generator controller

--- test/generator.rb
+++ test/generator.rb
@@ -7,17 +7,17 @@ class TestNephosServerGenerator < Test::Unit::TestCase
   end
 
   def test_generator_controller
-    `rm -f app/test.rb`
+    `rm -f app/test_controller.rb`
 
     `nephos-generator --debug c test`
-    assert File.exists? "app/test.rb"
-    assert_equal "class TestController < Nephos::Controller", File.read("app/test.rb").split("\n").first
-    `rm -f app/test.rb`
+    assert File.exists? "app/test_controller.rb"
+    assert_equal "class TestController < Nephos::Controller", File.read("app/test_controller.rb").split("\n").first
+    `rm -f app/test_controller.rb`
 
     `nephos-generator c test --debug`
-    assert File.exists? "app/test.rb"
-    assert_equal "class TestController < Nephos::Controller", File.read("app/test.rb").split("\n").first
-    `rm -f app/test.rb`
+    assert File.exists? "app/test_controller.rb"
+    assert_equal "class TestController < Nephos::Controller", File.read("app/test_controller.rb").split("\n").first
+    `rm -f app/test_controller.rb`
   end
 
   # test simple and rm
