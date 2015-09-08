5fdfd4d53e7125bcc134710938c3d8b09e49621a
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-09-08 23:52:48 +0200

    improve errors handled by the router

--- lib/nephos-server/router/main.rb
+++ lib/nephos-server/router/main.rb
@@ -29,22 +29,33 @@ module Nephos
       return {route: route, verb: verb, from: from, path: path, args: args}
     end
 
+    def self.error(code, err=nil)
+      if ENV["ENVIRONMENT"].to_s.match(/prod(uction)?/)
+        return render(status: code)
+      elsif err
+        #TODO: improve this
+        return render(status: code, content: err.is_a?(String) ? err : err.message)
+      else
+        return render(status: code)
+      end
+    end
+
     def self.execute(env)
       begin
         route = URI.parse(env['REQUEST_URI'])
       rescue => err
         puts "uri err #{err.message}".red
-        return render(status: 500)
+        return error(500, err)
       end
       parsed = parse_env(env, route)
       puts "#{parsed[:from]} [#{parsed[:verb]}] \t ---> \t #{route}"
       call = parse_path(parsed[:path], parsed[:verb])
-      return render(status: 404) if call.nil?
+      return error(404, "404 not found \"#{route}\"") if call.nil?
       begin
         controller = Module.const_get(call[:controller]).new(env, parsed, call)
-        return render(controller.send(call[:method]) || {status: 500})
+        return render(controller.send(call[:method]))
       rescue => err
-        return render(status: 500)
+        return error(500, err)
       end
     end
 
