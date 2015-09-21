699dbcde655e16a2804938b44df15355d579a246
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-09-21 14:16:19 +0200

    add lite documentation on Param

--- lib/nephos-server/params.rb
+++ lib/nephos-server/params.rb
@@ -1,8 +1,16 @@
 # coding: utf-8
 
 module Nephos
+
+  # Params to a hash, where every elements are accessibles via a stringified key
+  # so, even if the entry was added with the key :KEY, it will be accessible via
+  # a string equivalent to :key.to_s
+  #   param["key"] == param[:key]
+  #
+  # Every methods present in {Hash} are usable in {Param}.
   class Params
 
+    # @param hash [Hash] hash containing the parameters
     def initialize(hash={})
       raise ArgumentError, "the first argument must be a Hash" unless hash.is_a? Hash
       @hash = Hash[hash.map{|k,v| [k.to_s, v]}]
